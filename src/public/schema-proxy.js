38ef606e8473c7d8f8869359d21f41ce47d582d3
Greg Hurrell <greg@hurrell.net>
2017-01-23 17:51:09 -0800

    Add "build-public" script to build an independent static app
    Consists of:
    
    - Simple index.html file.
    - Main app bundle that loads dependencies and renders GraphiQL:
      - "fetch" and "react" load in parallel, then "react-dom", and finally
        "graphiql".
    - "schema" bundle that exposes the app schema, "graphql" executor, and
      cached data; this is loaded separately from within the GraphiQL
      fetcher callback.
    
    The main app bundle is about 56KB in size (15KB gzipped) and the
    schema/data bundle is 1.0MB (152KB gzipped).
    
    I'll have follow-up commits to this, including a script that actually
    deploys to the gh-pages branch, but first I have to investigating
    whether there'll be any conflict with our existing gh-pages for
    graphql.github.io.

---
+++ src/public/schema-proxy.js
@@ -0,0 +1,18 @@
+/**
+ *  Copyright (c) 2015, Facebook, Inc.
+ *  All rights reserved.
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE-examples file in the root directory of this source tree.
+ */
+
+import {graphql} from 'graphql';
+import schema from '../schema';
+
+export default {
+  execute(query, variables, operationName) {
+    return graphql(schema, query, null, null, variables, operationName);
+  },
+
+  schema,
+}
