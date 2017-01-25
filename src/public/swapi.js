ab59da4aedfb87d32e195e8907a87ef9d911a641
Greg Hurrell <greg@hurrell.net>
2017-01-25 11:04:46 -0800

    Add a missing license header

--- src/public/swapi.js
+++ src/public/swapi.js
@@ -1,3 +1,11 @@
+/**
+ *  Copyright (c) 2015, Facebook, Inc.
+ *  All rights reserved.
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE-examples file in the root directory of this source tree.
+ */
+
 (function () {
   'use strict';
 
