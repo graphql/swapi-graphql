75637d81b97d28de608c96fc489eec5208392461
Greg Hurrell <greg@hurrell.net>
2017-01-25 14:20:33 -0800

    Break download.sh across multiple lines
    For readability. Got rid of the `&&` because they were only being used
    to join the commands (`echo` can't really fail, so `;` should have been
    used to join anyway). Added `set -e` in any case, even though this
    script really runs only one real command; in the event that it fails, we
    want it to die noisily rather than printing a misleading "Cached!"
    message.

--- scripts/download.sh
+++ scripts/download.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-if [ ! -f src/api/cachedData/cache.js ]; then echo 'Downloading cache...' && node src/api/cachedData/downloadCache.js > src/api/cachedData/cache.js && echo 'Cached!'; fi;
+set -e
+
+if [ ! -f src/api/cachedData/cache.js ]; then
+  echo 'Downloading cache...'
+  node src/api/cachedData/downloadCache.js > src/api/cachedData/cache.js
+  echo 'Cached!'
+fi
