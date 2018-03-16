bb2d1a9737b550a85d9c23d9ad23d86d05c427e0
Ivan Goncharov <ivan.goncharov.ua@gmail.com>
2018-03-16 14:12:39 +0200

    Remove unused 'chai-subset' dependency

--- scripts/mocha-bootload.js
+++ scripts/mocha-bootload.js
@@ -7,11 +7,6 @@
  *  of patent rights can be found in the PATENTS file in the same directory.
  */
 
-var chai = require('chai');
-
-var chaiSubset = require('chai-subset');
-chai.use(chaiSubset);
-
 process.on('unhandledRejection', function(error) {
   console.error('Unhandled Promise Rejection:');
   console.error((error && error.stack) || error);
