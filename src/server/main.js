da49c79f9cf80146b55a836ff8ff0c7a7b0bf700
Ivan Goncharov <ivan.goncharov.ua@gmail.com>
2018-03-16 17:05:09 +0200

    Allow to provide port number through env.PORT

--- src/server/main.js
+++ src/server/main.js
@@ -24,13 +24,12 @@ app.use(
 );
 
 // Listen for incoming HTTP requests
-const listener = app.listen(() => {
+const listener = app.listen(process.env.PORT || undefined, () => {
   let host = listener.address().address;
   if (host === '::') {
     host = 'localhost';
   }
   const port = listener.address().port;
-  /* eslint-disable no-console */
+  // eslint-disable-next-line no-console
   console.log('Listening at http://%s%s', host, port === 80 ? '' : ':' + port);
-  /* eslint-enable no-console */
 });
