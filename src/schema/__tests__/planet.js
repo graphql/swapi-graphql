8fcd42e8dd339d63fb6f804a7eb700f44cda42c7
Ivan Goncharov <ivan.goncharov.ua@gmail.com>
2018-03-21 20:15:10 +0200

    Convert license formating to match 'graphql-js'

--- src/schema/__tests__/planet.js
+++ src/schema/__tests__/planet.js
@@ -1,6 +1,6 @@
 /**
- *  Copyright (c) 2015-present, Facebook, Inc.
- *  All rights reserved.
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
  *
  * This source code is licensed under the license found in the
  * LICENSE-examples file in the root directory of this source tree.
