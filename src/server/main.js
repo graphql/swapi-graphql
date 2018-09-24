ccab9858db4705648e61bf53e80544d1f0d330ad
Dávid Vass <david.vass@showmax.com>
2018-09-24 17:08:43 +0200

    Add posters

--- src/server/main.js
+++ src/server/main.js
@@ -12,10 +12,14 @@ import cors from 'cors';
 import express from 'express';
 import graphqlHTTP from 'express-graphql';
 import swapiSchema from '../schema';
+import path from 'path';
 
 const app = express();
 
+const PUBLIC_DIR = path.resolve(__dirname, '../../', 'public');
+
 app.use(cors());
+app.use(express.static(PUBLIC_DIR));
 
 // Requests to /graphql redirect to /
 app.all('/graphql', (req, res) => res.redirect('/'));
