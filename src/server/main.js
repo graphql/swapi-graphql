dfaf16bf5dcc1e0482635c71dd9006664a2cb66e
Rikki Schulte <richard.j.schulte@gmail.com>
2019-06-15 22:34:39 -0400

    serve schema statically, and the custom graphiql too

--- src/server/main.js
+++ src/server/main.js
@@ -9,6 +9,7 @@
  */
 
 import cors from 'cors';
+import fs from 'fs';
 import express from 'express';
 import graphqlHTTP from 'express-graphql';
 import swapiSchema from '../schema';
@@ -20,14 +21,22 @@ app.use(cors());
 // Requests to /graphql redirect to /
 app.all('/graphql', (req, res) => res.redirect('/'));
 
-app.use(
-  '/',
-  graphqlHTTP(() => ({
-    schema: swapiSchema,
-    // disable until we can get a new release of express-graphql with latest graphiql
-    // graphiql: true,
-  })),
-);
+// Load our own GraphiQL (since express-graphql has an older graphiql version)
+app.use('/graphiql', express.static('./public'));
+
+// Provide the static schema for reference in a few formats
+app.get('/schema', (req, res) => {
+  res.set('Content-Type', 'text');
+  fs.readFile('./schema.graphql', 'utf-8', (err, file) => {
+    res.write(Buffer.from(file));
+    res.end();
+  });
+});
+// octet-stream
+app.use('/schema.graphql', express.static('./schema.graphql'));
+
+// Finally, serve up the GraphQL Schema itself
+app.use('/', graphqlHTTP(() => ({ schema: swapiSchema })));
 
 // Listen for incoming HTTP requests
 const listener = app.listen(process.env.PORT || undefined, () => {
