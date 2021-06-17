3206d37bcd05cee0f2b6f7642ba13319cc4ed62f
Austin Testut <austintestut@gmail.com>
2021-06-17 15:51:41 -0500

    refactored to make gql call in App component and pass down info, allowing search bar to stay in header and have access to data

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,45 @@
-import CharacterContainer from  './CharacterContainer'
+import { useQuery, gql } from "@apollo/client";
+import CharacterContainer from "./CharacterContainer";
+import Header from "./Header";
+
+const GET_CHARACTERS = gql`
+  query GetPeople {
+    allPeople {
+      people {
+        name
+        birthYear
+        gender
+        homeworld {
+          name
+        }
+        species {
+          name
+        }
+      }
+    }
+  }
+`;
+
+const searchCharacters = (characters, query) => {
+  if (!query) {
+    return characters;
+  }
+  return characters.filter((char) => {
+    const name = char.name.toLowerCase();
+    return name.includes(query.toLowerCase());
+  });
+};
 
 function App() {
+  const { loading, error, data } = useQuery(GET_CHARACTERS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :</p>;
+
   return (
     <div className="App">
-      <CharacterContainer />
+      <Header searchCharacters={searchCharacters} />
+      <CharacterContainer people={data.allPeople.people} />
     </div>
   );
 }
--- client/src/CharacterContainer.js
+++ client/src/CharacterContainer.js
@@ -1,32 +1,8 @@
-import { useQuery, gql } from "@apollo/client";
 import CharacterCard from "./CharacterCard";
 
-const GET_CHARACTERS = gql`
-  query GetPeople {
-    allPeople {
-      people {
-        name
-        birthYear
-        gender
-        homeworld {
-          name
-        }
-        species {
-          name
-        }
-      }
-    }
-  }
-`;
-
-function CharacterContainer() {
-  const { loading, error, data } = useQuery(GET_CHARACTERS);
-
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error :</p>;
-
-  return data.allPeople.people.map((person) => {
-    return <CharacterCard person={person} key={person.name} />
+function CharacterContainer({ people }) {
+  return people.map((person) => {
+    return <CharacterCard person={person} key={person.name} />;
   });
 }
 
