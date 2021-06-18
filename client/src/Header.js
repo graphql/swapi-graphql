b7ffe447d522573a57cc3438ecb1c3a9ecb87b39
Austin Testut <austintestut@gmail.com>
2021-06-17 19:30:12 -0500

    basic styling of header, added border, centered elements

--- client/src/Header.js
+++ client/src/Header.js
@@ -1,11 +1,24 @@
+import styled from 'styled-components';
 import SearchBar from "./SearchBar";
+
+const StyledHeader = styled.div`
+  height: 60px;
+  border-bottom: 2px solid;
+  text-align: center;
+`;
+
+const StyledTitle = styled.span`
+  font-weight: 600;
+  font-family: Helvetica;
+  font-size: 20px;
+`;
 
 function Header({ updateSearch }) {
   return (
-    <div>
-      <span>Star Wars Characters</span>
+    <StyledHeader>
+      <StyledTitle>Star Wars Characters</StyledTitle>
       <SearchBar updateSearch={updateSearch} />
-    </div>
+    </StyledHeader>
   );
 }
 
